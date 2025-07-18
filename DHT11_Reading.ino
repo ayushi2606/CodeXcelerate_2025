aa06c81ed2d302219fefc0cfb3176d7e067fbff1
IEEE Sreyas Session <95907591+ayushi2606@users.noreply.github.com>
2025-07-18 14:20:23 +0530

    Add files via upload

---
+++ DHT11_Reading.ino
@@ -0,0 +1,27 @@
+#include <DHT.h>
+
+#define DHTPIN 4          
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.print(" °C, Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+  }
+
+  delay(2000);
+}
